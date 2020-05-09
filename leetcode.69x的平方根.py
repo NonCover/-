016a48407b7445eb53b236af1cac7944684f6231
NonCover <3471949741@qq.com>
2020-05-09 09:50:15 +0800

    牛顿迭代法+二分法

--- leetcode.69x的平方根.py
+++ leetcode.69x的平方根.py
@@ -20,26 +20,40 @@
 说明: 8 的平方根是 2.82842..., 
      由于返回类型是整数，小数部分将被舍去。
 '''         #Lang:python
-import time
 
-def Timer(mySqrt):
-    def runTime(x):
-        start = time.time()
-        mySqrt(x)
-        end = time.time()
-        print('运行时间:', end-start)
 
-    return runTime
-@Timer
-def mySqrt(x:int) -> int:
-    last = 1
-    cur = 0
-    while  True:
-        cur = last
-        last = (last + x / last) / 2            #牛顿迭代法     x=(x0 + a/x0)/2 求与x轴的焦点
-        if abs(cur - last) < 0.000006:
-            print('%s的平方根为%s'%(x, int(last)))
-            break
+class Solution:
+    def sqrt(self, x):
+        """
+        数学分析法，牛顿迭代法：
+        """
+        ## 牛顿迭代法
+        latest = 1
+        curr = 0
+        while True:
+            curr = latest
+            latest = (latest + x / latest) / 2  ## 迭代法公式
+            if (abs(latest - curr) < 0.0000001):
+                break
+
+        return int(curr)
+    def sqrt1(self, x):
+        """
+        二分法很好理解，要知道任何一个正整数的算术平方根是不可能大于这个数的一般的，所以基于这个特性，我们可以利用二分查找找[1, x / 2]那个数与x最接近。
+        注意的是，我们找中位数的时候，要找右中位数, 因为当遇到x = 9的时候，我们计算到区间[3, 4]，如果此时去中位数按照以前mid = （left + right） // 2的话，
+        此时mid = 3， 那么 mid * mid = 9 = x, 此时left = mid，区间始终在[3, 4]循环，所以要取右区间
+        """
+        ## 二分法
+        if x == 0: return 0
+        left, right = 1, x >> 1;
+        while left < right:
+            mid = (left + right + 1) >> 1
+            v = mid * mid
+            if v > x: right = mid - 1
+            else: left = mid + 1
+        return left
 
 if __name__ == "__main__":
-    mySqrt(8)
+    x = 9
+    out = Solution().sqrt1(x)
+    print(out)
